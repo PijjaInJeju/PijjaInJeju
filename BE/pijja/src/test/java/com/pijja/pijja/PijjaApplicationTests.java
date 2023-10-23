6590487cdec9205de4fb506db961cddc8207ecba
김선형 <tjsguddl96@gmail.com>
2023-10-23 12:01:03 +0900

    ADD : springboot pjt
    Former-commit-id: a0f167851e34de5c0adf11f1c51a7be0218779d4

---
+++ BE/pijja/src/test/java/com/pijja/pijja/PijjaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pijja.pijja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PijjaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
