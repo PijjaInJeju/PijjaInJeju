e38199c728d8488f25e9a0ca3b703403240b4cf2
YOOJUN <leehk77789@gmail.com>
2023-10-27 13:59:00 +0900

    Add) Companion save controller
    Former-commit-id: 04a4ad27f465721c219c77f27c0237f48e941a70

--- BE/pijja/src/main/java/com/A605/pijja/domain/member/controller/CompanionController.java
+++ BE/pijja/src/main/java/com/A605/pijja/domain/member/controller/CompanionController.java
@@ -1,14 +1,22 @@
 package com.A605.pijja.domain.member.controller;
 
+import com.A605.pijja.domain.member.dto.CompanionDto;
 import com.A605.pijja.domain.member.service.CompanionService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/companion")
+@RequestMapping("/api/companions")
 @RequiredArgsConstructor
 public class CompanionController {
-    private final CompanionService 
+    private final CompanionService companionService;
+
+    @PostMapping
+    public void saveCompanion(@RequestBody CompanionDto companionDto) {
+        companionService.saveCompanion(companionDto);
+    }
 
 }
