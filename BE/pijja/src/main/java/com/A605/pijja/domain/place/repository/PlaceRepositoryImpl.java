f99272b0b7ae695d47d9c8d3e2c8c7104136202f
전주영 <bnb3228@naver.com>
2023-10-27 17:54:04 +0900

    FIX : jpql properties
    Former-commit-id: 31fda5428ca4ae2ef4eb09a9c1227ea3b289a227

--- BE/pijja/src/main/java/com/A605/pijja/domain/place/repository/PlaceRepositoryImpl.java
+++ BE/pijja/src/main/java/com/A605/pijja/domain/place/repository/PlaceRepositoryImpl.java
@@ -1,6 +1,7 @@
 package com.A605.pijja.domain.place.repository;
 
 import com.A605.pijja.domain.place.entity.Place;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -9,7 +10,8 @@ import java.util.List;
 @Repository
 @RequiredArgsConstructor
 public class PlaceRepositoryImpl implements PlaceRepositoryCustom {
-    private final JPAQueryFac
+    private final JPAQueryFactory queryFactory;
+
     @Override
     public List<Place> allPlaces() {
         return null;
--- BE/pijja/src/main/java/com/A605/pijja/domain/place/service/PlaceServiceImpl.java
+++ BE/pijja/src/main/java/com/A605/pijja/domain/place/service/PlaceServiceImpl.java
@@ -1,6 +1,7 @@
 package com.A605.pijja.domain.place.service;
 
 import com.A605.pijja.domain.place.dto.response.AllPlacesResponseDto;
+import com.A605.pijja.domain.place.entity.Place;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,6 +14,7 @@ public class PlaceServiceImpl implements PlaceService {
     @Override
     @Transactional(readOnly = true)
     public List<AllPlacesResponseDto> allPlaces() {
+        List<Place> placeList;
         return null;
     }
 }
