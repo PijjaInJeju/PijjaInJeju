1f70a65a4a3b9c3d24af372a24c4be232807503b
김선형 <tjsguddl96@gmail.com>
2023-10-31 17:50:01 +0900

    MODIFY : get route tmap
    Former-commit-id: d38fffa83d6e591a7fb2d9d2a82897c7b386f34d

--- BE/pijja/src/main/java/com/A605/pijja/domain/plan/controller/PlanController.java
+++ BE/pijja/src/main/java/com/A605/pijja/domain/plan/controller/PlanController.java
@@ -83,7 +83,7 @@ public class PlanController {
     }
 
     @PostMapping("/getroute")
-    public ResponseEntity<String> getRouteTmap(@RequestBody List<SearchPlaceFromTmapRequestDto> requestDto){
+    public ResponseEntity<String> getRouteTmap(@RequestBody GetRouteTmapRequestDto requestDto){
         String tmapApiKey=tmapConfig.getTmapApiKey();
         String tmapUrl=tmapConfig.getTmapUrl();
         DefaultUriBuilderFactory factory=new DefaultUriBuilderFactory(tmapUrl);
@@ -94,70 +94,15 @@ public class PlanController {
                 .baseUrl(tmapUrl)
                 .build();
 
-        List<String> encodedPlaces=new ArrayList<>();
-        for(int i=0;i<requestDto.size();i++){
-            encodedPlaces.add(URLEncoder.encode(requestDto.get(i).getPlace(),StandardCharsets.UTF_8));
-        }
-        List<ResponseEntity<String>> resultList=new ArrayList<>();
-        for(int i=0;i<encodedPlaces.size();i++){
-            final int index = i;
-            resultList.add(wc.get()
-                    .uri(uriBuilder -> uriBuilder.path("/tmap/pois")
-                            .queryParam("searchKeyword",encodedPlaces.get(index))
-                            .queryParam("appKey",tmapApiKey)
-                            .build())
-                    .retrieve()
-                    .toEntity(String.class)
-                    .block());
-        }
-        // JSON 응답 문자열
-        List<String> jsonResponseList=new ArrayList<>();
-        for(int i=0;i<resultList.size();i++){
-            jsonResponseList.add(resultList.get(i).getBody());
-        }
-
-        // Jackson ObjectMapper를 사용하여 JSON 파싱
-        ObjectMapper objectMapper = new ObjectMapper();
-        try {
-            ArrayList<String> nameList=new ArrayList<>();
-            ArrayList<String> latList=new ArrayList<>(); //위도 y가 위도 (lat)
-            ArrayList<String> lonList=new ArrayList<>(); //경도 x가 경도 (lon)
-            float startY,startX,endY,endX;
-
-            for(int i=0;i<jsonResponseList.size();i++){
-                JsonNode jsonNode=objectMapper.readTree(jsonResponseList.get(i));
-                JsonNode nameNode=jsonNode.at("/searchPoiInfo/pois/poi/0/name");
-                JsonNode frontLatNode = jsonNode.at("/searchPoiInfo/pois/poi/0/frontLat");
-                JsonNode frontLonNode =jsonNode.at("/searchPoiInfo/pois/poi/0/frontLon");
-                nameList.add(nameNode.asText());
-                latList.add(frontLatNode.asText());
-                lonList.add(frontLonNode.asText());
-            }
-            startY=Float.parseFloat(latList.get(0));
-            startX=Float.parseFloat(lonList.get(0));
-            endY=Float.parseFloat(latList.get(1));
-            endX=Float.parseFloat(lonList.get(1));
-            System.out.println(startY+" "+startX+" "+endX+" "+endY);
-            GetRouteTmapRequestDto routeRequestDto= GetRouteTmapRequestDto.builder()
-                    .startY(startY)
-                    .startX(startX)
-                    .endY(endY)
-                    .endX(endX).build();
-
-            ResponseEntity<String> response=wc.post()
-                    .uri(uriBuilder -> uriBuilder.path("/tmap/routes")
-                            .build())
-                    .header("appKey",tmapApiKey)
-                    .bodyValue(routeRequestDto)
-                    .retrieve()
-                    .toEntity(String.class)
-                    .block();
-
-            return response;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
+        ResponseEntity<String> response=wc.post()
+                .uri(uriBuilder -> uriBuilder.path("/tmap/routes")
+                        .build())
+                .header("appKey",tmapApiKey)
+                .bodyValue(requestDto)
+                .retrieve()
+                .toEntity(String.class)
+                .block();
+        return response;
     }
 
 }
