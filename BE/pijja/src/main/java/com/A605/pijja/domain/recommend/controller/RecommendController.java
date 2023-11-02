f6a40b35e6ef935751818ab3aa11e66555d035f7
전주영 <bnb3228@naver.com>
2023-11-02 16:24:44 +0900

    FEAT : RecommendController
    Former-commit-id: 78fd8a11ea26ae54251d1bff749ddb4a31d934fc

--- BE/pijja/src/main/java/com/A605/pijja/domain/recommend/controller/RecommendController.java
+++ BE/pijja/src/main/java/com/A605/pijja/domain/recommend/controller/RecommendController.java
@@ -1,4 +1,23 @@
 package com.A605.pijja.domain.recommend.controller;
 
+import com.A605.pijja.domain.recommend.dto.response.RecommendResponseDto;
+import com.A605.pijja.domain.recommend.service.RecommendService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/recommends")
 public class RecommendController {
+    private final RecommendService recommendService;
+
+    @GetMapping("/{tag}&{mate}")
+    public List<RecommendResponseDto> recommendPlaces(@PathVariable String tag, @PathVariable String mate){
+        return recommendService.recommendPlace(tag, mate);
+    }
 }
