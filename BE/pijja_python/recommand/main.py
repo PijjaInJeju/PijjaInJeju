978a667e0894ccef9bf5f975816f3583c1062f83
chech2 <chaelim3396@gmail.com>
2023-11-03 17:51:17 +0900

    SAVE: 중간 저장
    Former-commit-id: 5fdb0361ef6e1dec36ae0f28eb73b6dd97e1e8d4

--- BE/pijja_python/recommand/main.py
+++ BE/pijja_python/recommand/main.py
@@ -17,6 +17,15 @@ class routgetRequestCommandDto(BaseModel):
 
 @app.get("/check")
 def test2(data: routgetRequestCommandDto):
- 
+   # totalDistance가 제공되면 1번은 수행할 필요 없음
+    # 1. start지점과,  end지점의 직선거리가 3km이상인지 아닌지 확인
+     
+    
+    # 2. 두 지점 사이의 거리가 3km이하인 경우  
+    if (data.totalDistance < 3000):
+
+    # 3. 두 지점 사이의 거리가 3km 초과인 경우  
+    else:
+         
 
     return data
