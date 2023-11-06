45f5b12e11c388f04805dcfe2677a157dfa32544
YOOJUN <leehk77789@gmail.com>
2023-11-06 17:18:37 +0900

    (BE) Feat : Add isStart & isEnd (time)
    Former-commit-id: 3a65e7b31328292551d4de0577e07a1b243b4a45

--- BE/pijja/src/main/java/com/A605/pijja/domain/member/service/CompanionJoinedListService.java
+++ BE/pijja/src/main/java/com/A605/pijja/domain/member/service/CompanionJoinedListService.java
@@ -1,11 +1,13 @@
 package com.A605.pijja.domain.member.service;
 
 import com.A605.pijja.domain.member.dto.SuccessResponseDto;
-import com.A605.pijja.domain.member.dto.response.CompanionListDto;
+import com.A605.pijja.domain.member.dto.response.MyCompanionListDto;
 import com.A605.pijja.domain.member.entity.Companion;
 import com.A605.pijja.domain.member.entity.Member;
 import com.A605.pijja.domain.member.repository.MemberCompanionRepository;
 import com.A605.pijja.domain.member.repository.MemberRepository;
+import com.A605.pijja.global.time.TimeUtil;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
@@ -19,27 +21,43 @@ import org.springframework.transaction.annotation.Transactional;
 public class CompanionJoinedListService {
 
     private final MemberRepository memberRepository;
-
     private final MemberCompanionRepository memberCompanionRepository;
 
-    public ResponseEntity getMyCompanions(Long memberId) {
-        Member member = memberRepository.findMemberById(memberId)
-                .orElse(null);
+    // TimeUtil 클래스를 주입받습니다.
+    private final TimeUtil timeUtil;
 
-        List<CompanionListDto> myCompanions = memberCompanionRepository.findByMember(member)
+    public ResponseEntity getMyCompanions(Long memberId) {
+        Member member = memberRepository.findMemberById(memberId).orElse(null);
+
+        List<MyCompanionListDto> myCompanions = memberCompanionRepository.findByMember(member)
                 .stream()
                 .map(memberCompanion -> {
                     Companion companion = memberCompanion.getCompanion();
-                    return CompanionListDto.builder()
+                    MyCompanionListDto.MyCompanionListDtoBuilder builder = MyCompanionListDto.builder()
                             .id(companion.getId())
                             .name(companion.getName())
-                            .isStart(companion.getIsStart())
-                            .isEnd(companion.getIsEnd())
                             .tendency(companion.getTendency())
                             .mate(companion.getMate())
                             .startTime(companion.getStartTime())
-                            .endTime(companion.getEndTime())
-                            .build();
+                            .endTime(companion.getEndTime());
+
+                    // 현재 시간을 가져옵니다.
+                    LocalDateTime currentTime = timeUtil.getCurrentLocalDateTime();
+
+                    // startTime과 endTime과 현재 시간을 비교하여 isStart 및 isEnd 값을 설정합니다.
+                    if (currentTime.isAfter(companion.getStartTime())) {
+                        builder.isStart(true);
+                    } else {
+                        builder.isStart(false);
+                    }
+
+                    if (currentTime.isAfter(companion.getEndTime())) {
+                        builder.isEnd(true);
+                    } else {
+                        builder.isEnd(false);
+                    }
+
+                    return builder.build();
                 })
                 .collect(Collectors.toList());
 
