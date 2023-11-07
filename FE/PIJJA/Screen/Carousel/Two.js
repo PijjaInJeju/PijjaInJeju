d78904e48697af47d837360b6cf3e2e5db426672
송진현 <dkdkdkal123@naver.com>
2023-11-07 09:57:49 +0900

    Feat : 메인페이지 캐로셀 적용.
    Former-commit-id: 03d778c30d98dafbb9aba0ad3600f1a468754175

---
+++ FE/PIJJA/Screen/Carousel/Two.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const Two = () =>{
+
+    return (
+        <View
+            style={{
+                width: '100%',
+                height: '40%',
+            }}
+        >
+
+            <Text>신나는 제주도 여행중이시군요!</Text>
+
+        </View>
+    );
+}
+export default Two;
