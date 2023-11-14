7d1a10ed2e0c74817efc6bf263fd3355e096ed8d
김선형 <tjsguddl96@gmail.com>
2023-11-14 09:36:45 +0900

    DELETE : import
    Former-commit-id: d9279442639b43f948e0b0f74e0a61ab863a1295

--- BE/pijja/src/main/java/com/A605/pijja/domain/plan/controller/PathController.java
+++ BE/pijja/src/main/java/com/A605/pijja/domain/plan/controller/PathController.java
@@ -2,23 +2,11 @@ package com.A605.pijja.domain.plan.controller;
 
 import com.A605.pijja.domain.plan.dto.request.*;
 import com.A605.pijja.domain.plan.dto.response.GetRouteResponseDto;
-import com.A605.pijja.domain.plan.dto.response.SearchPlaceFromTmapResponseDto;
 import com.A605.pijja.domain.plan.service.PathService;
-import com.A605.pijja.global.tmap.TmapConfig;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.util.DefaultUriBuilderFactory;
 
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
+
 import java.util.List;
 import java.util.PriorityQueue;
 
