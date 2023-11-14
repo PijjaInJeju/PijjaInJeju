c91fefa6cc070477c682968caf2300b59550b949
송진현 <dkdkdkal123@naver.com>
2023-11-14 16:49:40 +0900

    Feat : 추천 스케줄 와이어 프레임 완성.,
    Former-commit-id: 800cf8e0b15cdb79e840c6280cb12a075dc15c6d

--- FE/PIJJA/Screen/RecommendSchedule.js
+++ FE/PIJJA/Screen/RecommendSchedule.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   SafeAreaView,
   View,
   SectionList,
+  FlatList,
   Text,
   StyleSheet,
   TouchableOpacity,
@@ -10,6 +11,8 @@ import {
 } from 'react-native';
 
 import Icon from 'react-native-vector-icons/AntDesign';
+
+import Carousel, { Pagination } from 'react-native-snap-carousel';
 
 const DATA = [
   {
@@ -128,102 +131,242 @@ const DATA = [
   },
 ];
 
-const Item = ({ id, title, address, longitude, latitude, navigation }) => (
-  <View
-    style={{
-      alignItems: 'center',
-      marginTop: 0,
-    }}
-  >
-    <View style={styles.stick} />
-    <TouchableOpacity
-      onPress={() => {
-        console.log(id, title, address, '클릭되었습니다.');
-        Linking.openURL(`geo:0,0?q=${encodeURIComponent(address)}`) 
-      }}
-      style={{
-        borderWidth: 1,
-        width: 300,
-        borderRadius: 10,
-      }}
-    >
-      <Text style={styles.title}>{title}</Text>
-      <Text style={styles.address}>{address}</Text>
-    </TouchableOpacity>
-    <View style={styles.stick} />
-    <Icon
-      name="pluscircleo"
-      size={30}
-      color="#000000"
-      onPress={() => console.log('추천해주세요')}
-    />
-  </View>
-);
+
 
 const RecommendSchedule = ({ route }) => {
+
+  const [selectDay,setSelectDay] = useState(DATA[0].title);
+
+  const [schedule,setSchedule] = useState(DATA[0].data);
+
   const renderSchedule = ({ item }) => (
     <scheduleItem title={item.title} address={item.address} />
   );
 
-  DATA[0].data = route.params.scheduleList;
-  console.log(route.params.scheduleList);
+  const Item = ({ item }) => (
+    <View
+      style={{
+        alignItems: 'center',
+        marginTop: 0,
+      }}
+    >
+      {
+        item === schedule[0]
+        ?
+          undefined
+        :
+          <View style={styles.stick} />
+      }
+      <TouchableOpacity
+        onPress={() => {
+          console.log(item.id, item.title, item.address, '클릭되었습니다.');
+          Linking.openURL(`geo:0,0?q=${encodeURIComponent(item.address)}`) 
+        }}
+        style={{
+          borderWidth: 1,
+          width: 300,
+          borderRadius: 10,
+        }}
+      >
+        <Text style={styles.title}>{item.title}</Text>
+        <Text style={styles.address}>{item.address}</Text>
+      </TouchableOpacity>
+      {
+        item === schedule[schedule.length - 1]
+        ?
+          undefined
+        :
+          <View style={styles.stick} />
+      }
+      {
+        item === schedule[schedule.length - 1]
+        ?
+          undefined
+        :
+          <Icon
+            name="pluscircleo"
+            size={30}
+            color="#000000"
+            onPress={() => console.log('추천해주세요')}
+          />
+      }
+      
+    </View>
+  );
+
+  //console.log(route.params.scheduleList);
+
+
 
   return (
     <SafeAreaView style={[styles.container]}>
-      <SectionList
-        sections={DATA}
-        keyExtractor={(item, index) => item + index}
-        renderItem={({ item }) => (
-          <Item id={item.id} title={item.title} address={item.address} longitude={item.longitude} latitude={item.latitude}/>
-        )}
-        renderSectionHeader={({ section: { title, start, end } }) => (
-          <View
-            style={{
-              alignItems: 'center',
-            }}
-          >
-            {start === false ? <View style={styles.stick} /> : undefined}
-            <Text
-              style={[
-                styles.day,
-                {
-                  borderWidth: 1,
-                  width: 100,
-                  borderRadius: 10,
-                },
-              ]}
-            >
-              {title}
-            </Text>
-            {end === false ? (
-              <View
-                style={{
-                  alignItems: 'center',
-                }}
-              >
-                <View style={styles.stick} />
-                <Icon
-                  name="pluscircleo"
-                  size={30}
-                  color="#000000"
-                  onPress={() => console.log('추천해주세요')}
-                />
-              </View>
-            ) : (
-              <></>
-            )}
-          </View>
-        )}
-      />
-      <TouchableOpacity
-        //style={styles.buttonNext}
-        onPress={() => {
-          //setTravelStyle();
-          //navigation.navigate('CreateScheduleMap');
+      <FlatList
+        style={{
+          height: screenHeight * 0.09,
         }}
-      >
-        <Text>완료</Text>
-      </TouchableOpacity>
+        horizontal={true}
+        data={DATA}
+        renderItem={
+          ({ item }) => {
+            return (
+              <View>
+                {
+                  item.title === selectDay
+                  ?
+                  <View
+                    style={{
+                      flexDirection: 'row',
+                      alignItems:'center',
+                      width: screenWidth * 0.20,
+                      height: screenHeight * 0.05,
+                      marginTop: 5,
+                      marginLeft: 5,
+                      marginRight: 5,
+                      borderTopRightRadius: 10,
+                      borderTopLeftRadius: 10,
+                      borderTopWidth: 2,
+                      borderLeftWidth: 2,
+                      borderRightWidth: 2,
+                    }}
+                  >
+                    <Text
+                      style={{
+                        textAlign: 'center',
+                        borderColor: '#000000',
+                        // -4 border Width가 각 2씩 * 2
+                        width: (screenWidth * 0.20) - 4,
+                        color: "#000000"
+                      }}
+                      onPress={
+                        () => {
+                          console.log(item.title, "   ", selectDay);
+                          setSelectDay(item.title);
+                          setSchedule(item.data);
+                        }
+                      }
+                    >
+                      {item.title}
+                    </Text>
+                  </View>
+                  :
+                  <View
+                    style={{
+                      flexDirection: 'row',
+                      alignItems:'center',
+                      width: screenWidth * 0.20,
+                      height: screenHeight * 0.05,
+                      marginTop: 5,
+                      marginLeft: 5,
+                      marginRight: 5,
+                      borderRadius: 10,
+                      borderWidth: 2,
+                    }}
+                  >
+                    <Text
+                      style={{
+                        textAlign: 'center',
+                        borderColor: '#000000',
+                        // -4 border Width가 각 2씩 * 2
+                        width: (screenWidth * 0.20) - 4,
+                        color: "#000000"
+                      }}
+                      onPress={
+                        () => {
+                          console.log(item.title, "   ", selectDay);
+                          setSelectDay(item.title);
+                        }
+                      }
+                    >
+                      {item.title}
+                    </Text>
+                  </View>
+                }
+                <View
+                  style={{
+                    height: 10,
+                  }}
+                >
+                  {
+                    item.title === selectDay
+                    ?
+                    <View
+                      style={{
+                        marginLeft: 5,
+                        marginRight: 5,
+                        borderLeftWidth: 2,
+                        borderRightWidth: 2,
+                        height: 10,
+                      }}
+                    />
+                    :
+                    <View
+                      style={{
+                        height: 10,
+                      }}
+                    />
+                  }
+                </View>
+                <View
+                  style={{
+                    height: 10,
+                  }}
+                >
+                  {
+                    item.title === selectDay
+                    ?
+                    <View
+                      style={{
+                        flexDirection: 'row',
+                        width: screenWidth * 0.2,
+                      }}
+                    >
+                      <View
+                        style={{
+                          // 5 패딩 + 2 borderWidth
+                          width: 5 + 2,
+                          height: 10,
+                          borderTopWidth: 2,
+                        }}
+                      />
+                      <View
+                        style={{
+                          height: 10,
+                          width: (screenWidth * 0.2) - 4,
+                        }}
+                      />
+                      <View
+                        style={{
+                          // 5 패딩 + 2 borderWidth +  1 부동소수점 오차 극복
+                          width: 5 + 3 + 1,
+                          height: 10,
+                          borderTopWidth: 2,
+                        }}
+                      />
+                    </View>
+                    :
+                    <View
+                      style={{
+                        borderTopWidth: 2,  
+                      }}
+                    />
+                  }
+                </View>
+              </View>
+            )
+          }
+        }
+      />
+      {console.log(schedule)}
+      <FlatList
+        style={{
+          height: screenHeight * 0.9,
+          width: screenWidth,
+        }}
+        data={schedule}
+        renderItem={Item}
+        keyExtractor={item => item.id}
+      />
+      
     </SafeAreaView>
   );
 };
