5753acbd0c427bb1487bacc3f74999e2ab98ce7d
김선형 <tjsguddl96@gmail.com>
2023-11-16 11:08:27 +0900

    ADD : plandetail controller
    Former-commit-id: aad28bff9b4e209de5d5c5aa739c251e94ad6e82

--- BE/pijja/src/main/java/com/A605/pijja/domain/plan/repository/DayPlanRepositoryImpl.java
+++ BE/pijja/src/main/java/com/A605/pijja/domain/plan/repository/DayPlanRepositoryImpl.java
@@ -1,9 +1,22 @@
 package com.A605.pijja.domain.plan.repository;
 
+import com.A605.pijja.domain.plan.entity.DayPlan;
+import com.A605.pijja.domain.plan.entity.QDayPlan;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
 public class DayPlanRepositoryImpl implements DayPlanRepositoryCumstom{
+    private final JPAQueryFactory queryFactory;
+    private final QDayPlan dayPlan=new QDayPlan("dayPlan");
+    @Override
+    public List<DayPlan> findDayPlanListByPlanId(Long id) {
+        return queryFactory.selectFrom(dayPlan)
+                .where(dayPlan.plan.id.eq(id))
+                .fetch();
+    }
 }
